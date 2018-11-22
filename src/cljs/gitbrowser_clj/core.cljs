957fffc738a364a94af0a23263247a11fb02cbdb
nikonyrh <niko.nyrhila@gmail.com>
2018-11-22 22:58:56 +0200

    Added commit search by a query string

--- src/cljs/gitbrowser_clj/core.cljs
+++ src/cljs/gitbrowser_clj/core.cljs
@@ -22,6 +22,13 @@
    :hash->commit    (atom nil)
    :commit->parents (atom nil)})
 
+(def commit-search-string (atom ""))
+
+
+(defn args->query [args]
+  (->> (for [[k v] args :when v] (str (name k) "=" v))
+       (clojure.string/join "&")))
+
 
 (let [{:keys [repos repo-refs hash->commit commit->parents]} state]
   
@@ -32,11 +39,10 @@
   
   (defn reload-refs! [repo]
     (go (let [repo-name (:name repo)
-              refs      (-> repo :urls :refs (str "?to=100") http/get <! :body :response :refs)]
+              refs      (-> repo :urls :refs (str "?to=200") http/get <! :body :response :refs)]
           (swap! repo-refs assoc (:name repo) refs)
           (doseq [ref refs]
             (swap! hash->commit assoc [repo-name (:hash ref)] ref)))))
-  
   
   (defn reload-repos! []
     (reset! repos [])
@@ -44,31 +50,31 @@
     (reset! hash->commit {})
     (reset! commit->parents {})
     
-    (go (let [response (-> "/repos" http/get <! :body :response :repos)]
-          (reset! repos response)
-          (doseq [repo response]
+    (go (let [repos_ (-> "/repos" http/get <! :body :response :repos)]
+          (reset! repos repos_)
+          (doseq [repo repos_]
             (reload-refs! repo)))))
   
   
   (defn reload-commit! [repo-name hash]
-  ; (.log js/console (clojure.string/join " / " ["reload-commit!" repo-name hash]))
-    (swap! commit->parents dissoc [repo-name hash])
+   ;(swap! commit->parents dissoc [repo-name hash])
     
-    (go 
-      (let [current-hash->commit @hash->commit]
-        (when-let [commit (get current-hash->commit [repo-name hash])]
-          (let [parents (-> commit :urls :parents (str "?to=50") http/get <! :body :response :parents)
-                
-                new-commits
-                (for [commit parents
-                      :let  [commit-key [repo-name (:hash commit)]]
-                      :when (not (contains? current-hash->commit commit-key))]
-                  [commit-key commit])]
-            
-            (swap! commit->parents assoc [repo-name hash] parents)
-            
-            (when-not (empty? new-commits)
-              (swap! hash->commit merge (into {} new-commits)))))))))
+    (let [current-hash->commit @hash->commit]
+      (when-let [commit (get current-hash->commit [repo-name hash])]
+        (go (let [search @commit-search-string
+                  args    (args->query {:to 50 :query (when-not (empty? search) search)})
+                  parents (-> commit :urls :parents (str "?" args) http/get <! :body :response :parents)
+                  
+                  new-commits
+                  (for [commit parents
+                        :let  [commit-key [repo-name (:hash commit)]]
+                        :when (not (contains? current-hash->commit commit-key))]
+                    [commit-key commit])]
+              
+              (swap! commit->parents assoc [repo-name hash] parents)
+              
+              (when-not (empty? new-commits)
+                (swap! hash->commit merge (into {} new-commits)))))))))
 
 
 (defonce _
@@ -93,7 +99,7 @@
 
 (defn commit-col [urls repo-name hash & [ref-name]]
   [:span
-   [:a {:href (:self urls) :class "external" :target "_blank"} "^"] " "
+   [:a {:href (str (:self urls) "?pretty=1") :class "external" :target "_blank"} "^" " "]
    [:a {:href (str "/ui/repo/" repo-name "/commits/" hash)} (short-hash hash)]
    (when ref-name (str "   " (-> ref-name (clojure.string/replace #".+/" "") (str-subs 16))))])
 
@@ -101,7 +107,7 @@
 (let [{:keys [page repos repo-refs commit->parents]} state]
   
   (defn ref-list [repo-name n-refs]
-    [:pre {:style {:margin-left "1em"}}
+    [:pre {:class "content" :style {:margin-left "1em"}}
       [:b "YYYY-MM-DD HH:MM:SS | TYPE   | ^ COMMIT HASH @ REF NAME         | MSG\n"]
       (for [{:keys [msg ref-type name hash time urls]} (->> repo-name (get @repo-refs) (take n-refs))]
         ^{:key [repo-name hash]}
@@ -125,7 +131,8 @@
         ^{:key [repo-name (-> @repo-refs (get repo-name) first :hash)]}
         [:span
           [:h3 {:class "inactive"} "> " [:a {:href (str "/ui/repo/" repo-name)} repo-name]]
-          (conj (ref-list repo-name 5) "...")])))
+          (conj (ref-list repo-name 5)
+                [:a {:href (str "/ui/repo/" repo-name)} "..."])])))
   
   
   (defn render-repo []
@@ -140,41 +147,74 @@
     (let [{:keys [repo-name hash]} @page
           
           li           [:li {:style {:font-family "monospace" :whitespace "pre" :margin-left "1em"}}]
-          th           "YYYY-MM-DD HH:MM:SS | ^ COMMIT HASH | MSG"
+          th           "YYYY-MM-DD HH:MM:SS | ^ PARENT HASH | MSG"
           
           body-style   {:style {:margin-left (-> th count (+ 3) (str "ex")) :margin-right "1em" :margin-bottom "1em"}}
           inner-style  {:style {:font-size "75%"}}
           
           parents (get @commit->parents [repo-name hash])
           commit  (first parents)
-          max-body-rows 5]
-      ^{:key [:commit repo-name hash]}
+          
+          search @commit-search-string
+          has-search? (not-empty search)
+          
+          max-body-rows (if has-search? 1000 5)]
+      ^{:key [:commit repo-name hash]} ; (mapv :hash parents)]}
       [:div
         [:h3 {:class "inactive" :style {:margin-bottom "1em"}}
           [:a {:href "/"} "<"] " "
           [:a {:href (str "/ui/repo/" repo-name)} repo-name] " / " (short-hash hash)]
-       [:div {:style {:overflow-y "scroll" :height "8em" :border "0.1em solid #CCC" :background-color "#EEE"
-                      :margin-left "1.5em" :margin-right "4em"}}
-         [:pre {:style {:margin-top "0.3em" :padding-left "0.6em"}}
+       
+       [:div {:style {:overflow "scroll" :height "8em" :border "0.1em solid #CCC" :background-color "#EEE"
+                      :margin-left "1.5em" :margin-right "3em"}}
+         [:pre {:style {:margin-top "0.3em" :padding-left "0.6em" :overflow "show"}}
           (:msg commit)]]
+       
        [:ul {:style {:list-style-type "none" :padding-left "0px"}}
         (cons
-          (conj li [:b th "\n"])
+          (conj li
+            [:b th " "
+             [:input {:placeholder "Search by commit message" :size 40 :value @commit-search-string :on-change
+                      #(do (->> % .-target .-value (reset! commit-search-string))
+                           (reload-commit! repo-name hash))}]
+             "\n"])
           (for [{:keys [msg hash time urls]} parents
-                :let [[msg-title _ & msg-body] (clojure.string/split msg #"\r?\n")]]
+                :let [
+                      highlight-fn
+                      (if-not has-search? identity
+                        (let [regex (js/RegExp. (str "(" search ")") "i")
+                              padding "!@!@!@!@!@!@!@!@!@!"]
+                          ; TODO: Fix this hack :D How to split and keep "partitions"?
+                          (fn [s]
+                            (-> (str padding s)
+                                (clojure.string/replace regex (str padding "$1" padding))
+                                (clojure.string/split padding)
+                                rest
+                                (->> (map (fn [idx s]
+                                            (if (odd? idx) [:em s] s))
+                                       (range))
+                                     (into [:span]))))))
+                      [msg-title _ & msg-body] (-> msg (clojure.string/split #"\r?\n") (->> (map highlight-fn)))]]
             (into li
               (-> [(epoch->str time)
                    
                    (commit-col urls repo-name hash)
                    
-                   [:span msg-title [:br]
+                   [:span
+                    [:span msg-title [:br]
+                     
+                     (comment "I don't know how to get epplipsis on the commit message's title :("
+                       [:span {:style {:white-space "nowrap"}}
+                         [:div {:style {:text-overflow "ellipsis" :overflow "hidden" :display "inline"}}
+                           msg-title]])
+                     
                      (when msg-body
                        [:div body-style
                         (into [:span inner-style]
                           (-> (if (-> msg-body count (> max-body-rows))
                                 (concat (take (dec max-body-rows) msg-body) ["..."])
                                 msg-body)
-                              (my-interleave [:br])))])]]
+                              (my-interleave [:br])))])]]]
                   (my-interleave " | ")))))]]))
   
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -217,4 +257,3 @@
     
     (js/clearTimeout timeout)
     (re-render!)))
-
